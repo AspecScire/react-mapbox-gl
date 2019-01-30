ac9d2472a84401c5973c58006a650adf1c54aec8
Jibin George <hello@jibingeo.com>
2019-01-30 21:57:53 +0530

    fix forward ref

--- src/context.tsx
+++ src/context.tsx
@@ -7,11 +7,12 @@ export const MapContext = React.createContext(undefined) as React.Context<
 
 // tslint:disable-next-line:no-any
 export function withMap(Component: React.ComponentClass<any>) {
-  return function MappedComponent<T>(props: T) {
+  // tslint:disable-next-line:no-any
+  return React.forwardRef((props: any, ref: any) => {
     return (
       <MapContext.Consumer>
-        {map => <Component map={map} {...props} />}
+        {map => <Component map={map} ref={ref} {...props} />}
       </MapContext.Consumer>
     );
-  };
+  });
 }
